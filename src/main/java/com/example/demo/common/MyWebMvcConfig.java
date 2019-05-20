75f9a3398af1217765c0f65a5bd6cf2c0346dc35
WangMingZhi123 <Wmz19960224@163.com>
2019-05-20 20:20:45 +0800

    vue的代码提交

---
+++ src/main/java/com/example/demo/common/MyWebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *ClassDescription 用于springboot全局配置跨域类【之后接口就不需要在调用此注解[@CrossOrigin]了】
+ *@Author Wisdom
+ *@Date 2019/5/11 0011
+ *@return
+ **/
+@Configuration
+public class MyWebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .maxAge(1800)
+                .allowedOrigins("http://localhost:8099")
+                .allowedMethods("*");
+    }
+}
--- src/main/java/com/example/demo/controller/meun/GetMeunController.java
+++ src/main/java/com/example/demo/controller/meun/GetMeunController.java
@@ -3,10 +3,7 @@ package com.example.demo.controller.meun;
 import com.example.demo.entity.meun.meun;
 import com.example.demo.service.meun.meunService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,4 +17,9 @@ public class GetMeunController {
         return null;
     }
 
+    @PostMapping("/getMeun")
+    @CrossOrigin(value = "http://localhost:8099",maxAge = 1800,allowedHeaders = "*")
+    public String selectMeun(){
+        return "成功进入8091端口";
+    }
 }
